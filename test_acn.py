8a45badeabaa4acf442be07ba1c55f6856582ed2
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-09-28 14:48:50 +0200

    feat: acn automated download news in text file

--- test_acn.py
+++ test_acn.py
@@ -5,6 +5,8 @@ using Selenium IDE and Selenium Web Driver for Firefox.
 https://addons.mozilla.org/en-US/firefox/addon/selenium-ide/
 https://github.com/mozilla/geckodriver/releases/download/v0.27.0/geckodriver-v0.27.0-linux64.tar.gz
 
+Usage: pytest test_acn.py
+
 Author: https://github.com/aasensios
 '''
 
@@ -26,16 +28,34 @@ import os
 
 class TestAcn():
     def setup_method(self, method):
-        self.driver = webdriver.Firefox(executable_path=os.path.join(
-            Path().absolute(), 'geckodriver-v0.27.0-linux64/geckodriver'))
+        # Prevent download dialog
+        # https://stackoverflow.com/a/62254004
+        downloads_dir = os.path.join(Path().absolute(), 'downloaded_files')
+        options = webdriver.FirefoxOptions()
+        options.set_preference('browser.download.manager.showWhenStarting', False)
+        options.set_preference('browser.download.dir', downloads_dir)
+        options.set_preference('browser.download.folderList', 2)  # 0: Desktop, 1: Downloads, 2: custom directory
+        options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/html')  # Checked 'content-type: text/html' in Browser: F12 > Network > Headers
+        self.driver = webdriver.Firefox(
+            executable_path=os.path.join(Path().absolute(), 'geckodriver-v0.27.0-linux64/geckodriver'),
+            options=options,
+        )
         self.vars = {}
 
     def teardown_method(self, method):
         self.driver.quit()
 
-    def test_login(self):
+    def test_login_and_download_text_file(self):
         self.driver.get("https://www.acn.cat/")
+
+        # Login
         self.driver.find_element(By.LINK_TEXT, "Entra").click()
         self.driver.find_element(By.NAME, "username").send_keys("TEXT")
         self.driver.find_element(By.NAME, "password").send_keys("1865GB")
         self.driver.find_element(By.NAME, "Submit").click()
+
+        # Load specific page
+        self.driver.get("https://www.acn.cat/text/cultura/item/el-liceu-inaugura-temporada-amb-mesures-per-la-covid-19-i-es-retroba-amb-el-public-despres-de-sis-mesos")
+
+        # Trigger the download button for the clean text file
+        self.driver.find_element(By.CLASS_NAME, "uk-button").click()
