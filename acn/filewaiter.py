22d87a1e279c96c28b02bf02533674de7074df95
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-10-05 17:54:45 +0200

    fix: wait for the text file to be downloaded

---
+++ acn/filewaiter.py
@@ -0,0 +1,28 @@
+# https://stackoverflow.com/a/35925746
+
+import os, time, glob 
+
+class FileWaiter:
+
+    def __init__(self, path):
+        self.path = path
+        self.files = set(glob.glob(path))
+
+    def wait_new_file(self, timeout):
+        """
+        Waits for a new file to be created and returns the new file path.
+        """
+        endtime = time.time() + timeout
+        while True:
+            diff_files = set(glob.glob(self.path)) - self.files
+            if diff_files :
+                new_file = diff_files.pop()
+                try:
+                    os.rename(new_file, new_file)
+                    self.files = set(glob.glob(self.path))
+                    return new_file
+                except :
+                    pass
+            if time.time() > endtime:
+                    raise Exception("Timeout while waiting for a new file in %s" % self.path)
+            time.sleep(0.1)
