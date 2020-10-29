6042a179e2d5b7bc4eda4be3b8f539fd5e361ba6
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-10-29 21:57:01 +0100

    refactor: esco accepted term tsv builder

--- esco/esco_accepted_terms_builder.py
+++ esco/esco_accepted_terms_builder.py
@@ -1,20 +1,32 @@
+'''
+Converter from occupations csv into occupations tsv (code, acceptedTerm)
+that will contain several accepted terms per each code.
+
+Usage:
+    python esco_accepted_terms_builder.py with_codes_occupations_es.csv
+
+Author: https://github.com/mmaguero
+Refactorer: https://github.com/aasensios
+'''
+
 import pandas as pd
 import numpy as np
 import sys
 
 # args
-fileIn = sys.argv[1]
+input_file = sys.argv[1]
+output_file = f'{input_file.split('.')[0]}.tsv'
 
 # read data
-data = pd.read_csv(fileIn, encoding = 'utf8', sep=',')
-data['iscoGroup'] = data['iscoGroup'].apply(str)
+data = pd.read_csv(input_file, encoding = 'utf8', sep=',')
+data['code'] = data['code'].apply(str)
 
 # write output
 to_file = []
-with open(fileIn[:-4]+".tsv", "w+") as f:
-    
+with open(output_file, "w+") as f:
+
     for index, row in data.iterrows():
-    
+
         # desc columns: preferredLabel	altLabels	hiddenLabels
         desc = []
         if pd.notna(row['preferredLabel']): 
@@ -26,13 +38,12 @@ with open(fileIn[:-4]+".tsv", "w+") as f:
         if pd.notna(row['hiddenLabels']): 
             for lbl in row['hiddenLabels'].split('\n'):
                 desc.append(str(lbl).strip())
-        
+
         for d in desc:
             if len(d.strip())>0:
                 d = str(d).replace('/','') if d.strip().startswith('/') else d
-                to_file.append([str(row['iscoGroup']),str(d).strip()])
+                to_file.append([str(row['code']),str(d).strip()])
     
     unique_rows = np.unique(to_file, axis=0)
     for k, v in unique_rows:
         f.write("{}\t{}\n".format(k,v))
-        
