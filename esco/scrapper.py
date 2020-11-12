d44811266db25e5d580ca2a0957b187e8d59c465
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-11-12 16:33:55 +0100

    refactor: scrapper docstring and usage

--- esco/scrapper.py
+++ esco/scrapper.py
@@ -1,21 +1,17 @@
 '''
-Script to scrap the exact codes of every occupation from
+Scrap the exact (granular) code for every occupation in
 "European Skills/Competences, qualifications and Occupations" (ESCO) website:
 https://ec.europa.eu/esco/portal/occupation
 
-Previously, we have downloaded all occupations terms in Spanish via the portal:
+Previously, all occupations terms in Spanish have been downloaded from:
 https://ec.europa.eu/esco/portal/download
 
-File: ./occupations_es.csv
+For each row of the file occupations_es.csv, the script bypasses the ESCO's
+website redirections with a nice ad-hoc hack and extracts the exact ESCO code
+from the HTML (which is the first <p> element). Finally, a new csv file is
+written with an extra 'code' column.
 
-For each row of that csv file, the script visits the conceptUri (with a nice
-hack to avoid internal website redirections) and extracts the exact ESCO code
-from the webpage (that is the first <p> element).
-
-Finally, it writes a new csv file with an extra column for the ESCO code.
-
-Usage:
-    python test_esco.py occupations_es.csv
+Usage: python scrapper.py occupations_es.csv occupations_es_extra_code.csv
 
 Author: https://github.com/aasensios
 '''
@@ -29,12 +25,16 @@ from bs4 import BeautifulSoup
 
 BASE_URL = 'https://ec.europa.eu/esco/portal/occupation'
 input_filename = sys.argv[1]
-output_filename = f'with_codes_{input_filename}'
+output_filename = sys.argv[2]
 
 
-def scrap_esco_code(concept_uri):
+def scrap_esco_code(concept_uri: str) -> str:
+    '''Get the exact ESCO code from HTML page.'''
     safe_url = urllib.parse.quote(concept_uri, safe='')
+
+    # URL format after multiple redirections by ESCO website
     url = f'{BASE_URL}?uri={safe_url}&conceptLanguage=es&full=true#&uri={concept_uri}'
+
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     code = soup.p.text
@@ -42,7 +42,7 @@ def scrap_esco_code(concept_uri):
 
 with open(input_filename) as input_file:
     rows = list(csv.DictReader(input_file))
-    fieldnames = rows[0].keys()
+    fieldnames = list(rows[0].keys())
     fieldnames.append('code')
 
 with open(output_filename, 'w') as output_file:
@@ -53,9 +53,10 @@ for row in rows:
     concept_uri = row.get('conceptUri')
     code = scrap_esco_code(concept_uri)
     row['code'] = code
+
     with open(output_filename, 'a') as output_file:
         writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         writer.writerow(row)
 
-    # for visual progress in terminal
+    # visual progress in terminal
     print(code, row.get('preferredLabel'))
